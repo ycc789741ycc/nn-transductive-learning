de4d4845ca691d3d426d1c4a0142473b68417eee
Yoshi Gao <yoshi4868686@gmail.com>
2023-03-22 07:08:44 +0800

    Modify the model size.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+from pathlib import Path
 from typing import Dict, Text, Tuple
 
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ import numpy as np
 import pandas as pd
 import yaml
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
 from models.lightning import LightningTransductiveLearner
 
@@ -22,6 +24,8 @@ def create_argument_parser() -> argparse.ArgumentParser:
     parser.add_argument("-y", type=str, help="Y labeled data csv path, columns=y")
     parser.add_argument("-t", type=str, help="X unlabeled data, columns=X_0, X_1, X_2,...")
     parser.add_argument("-o", type=str, help="Outputs directory.", default=DEFAULT_OUTPUTS_DIR)
+    parser.add_argument("--pca", help="PCA visualization the data.", action="store_true")
+    parser.add_argument("--tsne", help="T-SNE visualization the data.", action="store_true")
 
     return parser
 
@@ -95,19 +99,6 @@ def get_labeled_data_from_transductive_learning(
     return X_model_labeled, y_model_labeled
 
 
-def data_visualization(
-    X_labeled: np.ndarray,
-    y_labeled: np.ndarray,
-    X_model_labeled: np.ndarray,
-    y_model_labeled: np.ndarray,
-    output_dir: Text,
-) -> None:
-    """data visualization."""
-
-    pca_visualization(X_labeled, y_labeled, X_model_labeled, y_model_labeled, output_dir)
-    tsne_visualization(X_labeled, y_labeled, X_model_labeled, y_model_labeled, output_dir)
-
-
 def pca_visualization(
     X_labeled: np.ndarray,
     y_labeled: np.ndarray,
@@ -118,7 +109,6 @@ def pca_visualization(
     """PCA visualization."""
 
     pca = PCA(n_components=3)
-    pca.fit(X_labeled)
     X_pca = pca.fit_transform(np.concatenate([X_labeled, X_model_labeled], axis=0))
 
     fig = plt.figure(figsize=(8, 6))
@@ -159,7 +149,7 @@ def pca_visualization(
 
     ax.set_title("PCA visualization")
     ax.legend()
-    fig.savefig("pca_visualization.png")
+    fig.savefig(Path(output_dir).joinpath("pca_visualization.png"))
 
 
 def tsne_visualization(
@@ -170,7 +160,49 @@ def tsne_visualization(
     output_dir: Text,
 ) -> None:
     """tsne visualization."""
-    pass
+
+    tsne = TSNE(n_components=3)
+    X_tsne = tsne.fit_transform(np.concatenate([X_labeled, X_model_labeled], axis=0))
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(projection="3d")
+    X_labeled_size = len(X_labeled)
+    ax.scatter(
+        X_tsne[:X_labeled_size, 0][y_labeled == 0],
+        X_tsne[:X_labeled_size, 1][y_labeled == 0],
+        X_tsne[:X_labeled_size, 2][y_labeled == 0],
+        alpha=0.2,
+        color="blue",
+        label="labeled data 0",
+    )
+    ax.scatter(
+        X_tsne[:X_labeled_size, 0][y_labeled == 1],
+        X_tsne[:X_labeled_size, 1][y_labeled == 1],
+        X_tsne[:X_labeled_size, 2][y_labeled == 1],
+        alpha=0.2,
+        color="red",
+        label="labeled data 1",
+    )
+    ax.scatter(
+        X_tsne[X_labeled_size:, 0][y_model_labeled == 0],
+        X_tsne[X_labeled_size:, 1][y_model_labeled == 0],
+        X_tsne[X_labeled_size:, 2][y_model_labeled == 0],
+        alpha=0.2,
+        color="purple",
+        label="model labeled data 0",
+    )
+    ax.scatter(
+        X_tsne[X_labeled_size:, 0][y_model_labeled == 1],
+        X_tsne[X_labeled_size:, 1][y_model_labeled == 1],
+        X_tsne[X_labeled_size:, 2][y_model_labeled == 1],
+        alpha=0.2,
+        color="orange",
+        label="model labeled data 1",
+    )
+
+    ax.set_title("TSNE visualization")
+    ax.legend()
+    fig.savefig(Path(output_dir).joinpath("tsne_visualization.png"))
 
 
 def main():
@@ -179,7 +211,10 @@ def main():
     X_model_labeld, y_model_labeled = get_labeled_data_from_transductive_learning(
         X_labeled, y_labeled, X_unlabeled, TRAINING_CONFIG_PATH, arg_parser.parse_args().o
     )
-    data_visualization(X_labeled, y_labeled, X_model_labeld, y_model_labeled, arg_parser.parse_args().o)
+    if arg_parser.parse_args().pca:
+        pca_visualization(X_labeled, y_labeled, X_model_labeld, y_model_labeled, arg_parser.parse_args().o)
+    if arg_parser.parse_args().tsne:
+        tsne_visualization(X_labeled, y_labeled, X_model_labeld, y_model_labeled, arg_parser.parse_args().o)
 
 
 def dev_pca():
@@ -195,6 +230,18 @@ def dev_pca():
     pca_visualization(X_labeled, y_labeled, X_model_labeled, y_model_labeled, "outputs")
 
 
+def dev_tsne():
+    X_labeled = pd.read_pickle("data/X_labeled.pkl").to_numpy(dtype=np.float32)
+    y_labeled = pd.read_pickle("data/y_labeled.pkl").to_numpy(dtype=np.float32)[:, 0]
+    X_model_labeled_df = pd.read_pickle("outputs/X_model_labeled.pkl")
+    X_model_labeled_df.pop("id")
+    X_model_labeled = X_model_labeled_df.to_numpy(dtype=np.float32)
+    y_model_labeled_df = pd.read_pickle("outputs/y_model_labeled.pkl")
+    y_model_labeled_df.pop("id")
+    y_model_labeled = y_model_labeled_df.to_numpy(dtype=np.float32)[:, 0]
+
+    tsne_visualization(X_labeled, y_labeled, X_model_labeled, y_model_labeled, "outputs")
+
+
 if __name__ == "__main__":
-    # main()
-    dev_pca()
+    main()
